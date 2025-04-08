94dbd5668e38496598e019fd5aa1e3c3e717961c
NikolasJBL <dimitris13@gmail.com>
2025-04-08 17:10:22 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
 basic.forever(function () {
 	
 })
+
+//% color=190 weight=100 icon="\uf293" block="Bluetooth"
+namespace bluetooth {
+    /**
+     * Enable Bluetooth on the micro:bit.
+     */
+    //% block="enable Bluetooth"
+    export function enableBluetooth(): void {
+        control.enableBluetooth();
+    }
+}
